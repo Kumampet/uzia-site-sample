893daa94fa59e2eda7acdf5ab9d3c78185a7ba4c
Kumampet <kumampet@gmail.com>
2023-01-08 17:13:37 +0900

    不要ファイル削除, fix

--- uzia-site-sample/src/components/index.js
+++ uzia-site-sample/src/components/index.js
@@ -5,7 +5,6 @@ export { default as CustomFooter } from './CustomFooter';
 export { default as CustomCarousel } from './CustomCarousel';
 export { default as Home } from './Home';
 export { default as HomeContentRow } from './HomeContentRow';
-export { default as LatestContentsPanel } from './LatestContentsPanel';
 export { default as CardTile } from './CardTile';
 export { default as About } from './About';
 export { default as Game } from './Game';
