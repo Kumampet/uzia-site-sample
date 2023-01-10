706ccb65e1b4a13c3924ed704ddd6d457a746b33
Kumampet <kumampet@gmail.com>
2023-01-11 04:37:13 +0900

    news一覧対応

--- uzia-site-sample/src/components/NewsGallery.js
+++ uzia-site-sample/src/components/NewsGallery.js
@@ -1,27 +1,53 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import AppContext from '../AppContext';
+import { ListGroup, Row, Col } from 'react-bootstrap';
+import dayjs from 'dayjs';
+import ja from 'dayjs/locale/ja';
+
+import _get from 'lodash/get';
+
+// dayjsの日本語対応
+dayjs.locale(ja);
 
 class NewsGallery extends React.Component {
+  static contextType = AppContext;
+
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  genearteNewsGallery = (items = []) => {
+    return [...items].map(item => {
+      const id = _get(item, 'id');
+      const title = _get(item, 'title');
+      const summary = _get(item, 'summary');
+      const href = `${process.env.PUBLIC_URL}/news/${id}`;
+      const updateDate = _get(item, 'update_date');
+      return (
+        <ListGroup.Item action href={href} className="news-list-group-contents">
+          <div className="d-flex justify-content-between align-self-end">
+            <div>
+              <h3>{title}</h3>
+              {updateDate && (
+                <span className="fs-6">更新日時: {dayjs(updateDate).format('YYYY年MMMMD日')}</span>
+              )}
+              <p>{summary}</p>
+            </div>
+            <div className="ps-3 text-nowrap" style={{ placeSelf: "end"}}>
+              <p>read more →</p>
+            </div>
+          </div>
+        </ListGroup.Item>
+      );
+    });
   }
 
   render() {
     return (
       <React.Fragment>
         <ListGroup>
-          <ListGroup.Item action href="#link1">
-            Link 1
-          </ListGroup.Item>
-          <ListGroup.Item action href="#link2" disabled>
-            Link 2
-          </ListGroup.Item>
-          <ListGroup.Item action onClick={() => {
-            alert('You clicked the third ListGroupItem');
-          }}>
-            This one is a button
-          </ListGroup.Item>
+          {this.genearteNewsGallery(this.context.newsDataItems)}
         </ListGroup>
       </React.Fragment>
     )
