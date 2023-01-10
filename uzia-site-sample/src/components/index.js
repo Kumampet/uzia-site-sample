f3b1c6591981b600eff9ccb3ba0dfd51706ddd47
Kumampet <kumampet@gmail.com>
2023-01-11 03:02:34 +0900

    不要コンポーネント削除

--- uzia-site-sample/src/components/index.js
+++ uzia-site-sample/src/components/index.js
@@ -14,5 +14,4 @@ export { default as Contact } from './Contact';
 export { default as PageHeaderImage } from './PageHeaderImage';
 export { default as AboutDevelopMember } from './AboutDevelopMember';
 export { default as TwitterEmbed } from './TwitterEmbed';
-export { default as CustomTwitterDMButton } from './CustomTwitterDMButton';
 export { default as HomeContact } from './HomeContact';
