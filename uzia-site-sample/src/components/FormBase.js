155416fd15a8348929dd6eaf0d7f65c5f63490b6
Kumampet <kumampet@gmail.com>
2023-01-13 09:44:46 +0900

    メールバリデーション追加。テンプレート変数に対応

--- uzia-site-sample/src/components/FormBase.js
+++ uzia-site-sample/src/components/FormBase.js
@@ -9,12 +9,20 @@ const PUBLIC_KEY = 'F1LdMvCn6OaB4rwCG';
 class FormBase extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      label: {
+        name: 'お名前',
+        email: 'メールアドレス',
+        subject: '件名',
+        message: 'お問い合わせ内容',
+        submit: '送信'
+      }
+    };
     this._formRef = React.createRef();
   }
 
   onChangeValue = (e) => {
-    console.log({e})
+    console.log({ e })
     // const target = e.target;
   }
 
@@ -23,51 +31,85 @@ class FormBase extends React.Component {
 
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, this._formRef.current, PUBLIC_KEY)
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   }
 
   render() {
+    const { label } = this.state;
     return (
       <Form ref={this._formRef} onSubmit={this.onSubmit}>
         <Form.Group className="mb-3">
           <FloatingLabel
             controlId="floatingInput"
-            label="お名前"
+            label={label.name}
             className="mb-3"
           >
-            <Form.Control onChange={this.onChangeValue} name="name" type="name" placeholder="お名前" />
+            <Form.Control
+              required
+              onChange={this.onChangeValue}
+              name="name"
+              type="text"
+              placeholder={label.name}
+            />
+            <Form.Control.Feedback type="invalid">{label.name}は必須項目です。</Form.Control.Feedback>
           </FloatingLabel>
         </Form.Group>
-
         <Form.Group className="mb-3">
           <FloatingLabel
             controlId="floatingMail"
-            label="Email"
+            label={label.email}
             className="mb-3"
           >
-            <Form.Control onChange={this.onChangeValue} name="email" type="email" placeholder="mail@example.com" />
+            <Form.Control
+              required
+              onChange={this.onChangeValue}
+              name="email"
+              type="email"
+              placeholder="mail@example.com"
+            />
+            <Form.Control.Feedback type="invalid">{label.email}は必須項目です。</Form.Control.Feedback>
           </FloatingLabel>
         </Form.Group>
-
         <Form.Group className="mb-3">
-          <FloatingLabel controlId="floatingTextarea" label="お問い合わせ内容">
+          <FloatingLabel
+            controlId="floatingInput"
+            label={label.subject}
+            className="mb-3"
+          >
             <Form.Control
+              required
+              onChange={this.onChangeValue}
+              name="subject"
+              type="text"
+              placeholder="件名を入力してください。"
+            />
+            <Form.Control.Feedback type="invalid">{label.subject}は必須項目です。</Form.Control.Feedback>
+          </FloatingLabel>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <FloatingLabel
+            controlId="floatingTextarea"
+            label={label.message}
+          >
+            <Form.Control
+              required
               as="textarea"
-              type="value"
+              type="text"
               name="message"
-              onChange={this.onChangeValue} 
-              placeholder="お問い合わせ内容"
+              onChange={this.onChangeValue}
+              placeholder="お問い合わせ内容を入力してください。"
               style={{ height: '100px' }}
             />
+            <Form.Control.Feedback type="invalid">{label.message}は必須項目です。</Form.Control.Feedback>
           </FloatingLabel>
         </Form.Group>
 
         <Form.Group className="mb-3">
           <div className="d-grid gap-2">
-            <Button type="submit" size="lg">送信</Button>
+            <Button type="submit" size="lg">{label.submit}</Button>
           </div>
         </Form.Group>
       </Form>
