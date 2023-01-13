021ce50ffd72b3f10bcd3a1fa4cf9517c3df57fc
Kumampet <kumampet@gmail.com>
2023-01-13 09:23:20 +0900

    メール送信の土台

--- uzia-site-sample/src/components/FormBase.js
+++ uzia-site-sample/src/components/FormBase.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import { Form, Button, FloatingLabel } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
+
+const SERVICE_ID = 'service_donop5o';
+const TEMPLATE_ID = 'template_ahpgmfe';
+const PUBLIC_KEY = 'F1LdMvCn6OaB4rwCG';
 
 class FormBase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this._formRef = React.createRef();
+  }
+
+  onChangeValue = (e) => {
+    console.log({e})
+    // const target = e.target;
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, this._formRef.current, PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   render() {
     return (
-      <Form>
+      <Form ref={this._formRef} onSubmit={this.onSubmit}>
         <Form.Group className="mb-3">
           <FloatingLabel
             controlId="floatingInput"
             label="お名前"
             className="mb-3"
           >
-            <Form.Control type="name" placeholder="お名前" />
+            <Form.Control onChange={this.onChangeValue} name="name" type="name" placeholder="お名前" />
           </FloatingLabel>
         </Form.Group>
 
@@ -21,7 +48,7 @@ class FormBase extends React.Component {
             label="Email"
             className="mb-3"
           >
-            <Form.Control type="email" placeholder="mail@example.com" />
+            <Form.Control onChange={this.onChangeValue} name="email" type="email" placeholder="mail@example.com" />
           </FloatingLabel>
         </Form.Group>
 
@@ -30,6 +57,8 @@ class FormBase extends React.Component {
             <Form.Control
               as="textarea"
               type="value"
+              name="message"
+              onChange={this.onChangeValue} 
               placeholder="お問い合わせ内容"
               style={{ height: '100px' }}
             />
