44847d69fe853fec5a928200fb12a4f0fef3ad16
Kumampet <kumampet@gmail.com>
2023-01-15 12:19:28 +0900

    fix

--- uzia-site-sample/src/components/Game.js
+++ uzia-site-sample/src/components/Game.js
@@ -15,11 +15,11 @@ class Game extends React.Component {
     const { pageTitle } = this.state;
     return (
       <React.Fragment>
-        <PageHeaderImage pageTitle={pageTitle} backgroundImageSource={`${process.env.PUBLIC_URL}/img/page_header/kawa.png`} />
+        <PageHeaderImage pageTitle={pageTitle} backgroundImageSource={replaceURLPublicPath(`/img/page_header/kawa.png`)} />
         <Container>
           <div className="mt-5">
             {/* <h1>工事中</h1> */}
-            <GameGallery allViewPath={replaceURLPublicPath(`${process.env.PUBLIC_URL}/game`)} />
+            <GameGallery allViewPath={replaceURLPublicPath(`/game`)} />
           </div>
         </Container>
       </React.Fragment>
